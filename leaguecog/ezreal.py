ec28a60dabaa96eb1980be9e21fc55ed2438f8c1
Neil Hargis <neil.j.hargis@gmail.com>
2022-07-16 09:15:53 -0500

    Adding version handling for data dragon, and handling empty teams

--- leaguecog/ezreal.py
+++ leaguecog/ezreal.py
@@ -52,29 +52,43 @@ class Ezreal(MixinMeta):
         self, summoner_name, game_type, champ_name, team1, team2, timestamp
     ):
         log.debug("Building embed")
+        version = self.champ_api_version
         embed = discord.Embed()
         embed.title = f"{summoner_name} has started a {game_type} game!"
         embed.color = 0x00FF00
         embed.set_thumbnail(
-            url=f"http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/{champ_name}.png"
+            url=f"http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champ_name}.png"
         )
         teamComp1 = ""
         teamComp2 = ""
+
+        # Turn teams into strings
         for champ in team1:
             teamComp1 = teamComp1 + str(team1[champ]) + " "
         for champ in team2:
             teamComp2 = teamComp2 + str(team2[champ]) + " "
-        embed.add_field(name="Blue Team", value=teamComp1)
-        embed.add_field(name="Red Team", value=teamComp2)
+
+        # If a team is empty (bots don't count), don't fail out.
+        if team1:
+            embed.add_field(name="Blue Team", value=teamComp1)
+        else:
+            embed.add_field(name="Blue Team", value="No teammates.")
+
+        if team2:
+            embed.add_field(name="Red Team", value=teamComp2)
+        else:
+            embed.add_field(name="Red Team", value="No teammates.")
+
         embed.timestamp = datetime.utcfromtimestamp(timestamp / 1000)
         return embed
 
     async def build_end_game(self, summoner_name, champ_name, team1, team2):
+        version = self.champ_api_version
         embed = discord.Embed()
         embed.title = f"{summoner_name}'s game has ended."
         embed.color = 0xFF0000
         embed.set_thumbnail(
-            url=f"http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/{champ_name}.png"
+            url=f"http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champ_name}.png"
         )
         embed.timestamp = datetime.utcnow()
         return embed
